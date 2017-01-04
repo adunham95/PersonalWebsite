fabc441d489a143d2fc47f2d829da7127dd61f16
adunham95 <adunham95@gmail.com>
2017-01-03 23:47:13 -0500

    Updated contact card

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
  * Created by apdun on 1/1/2017.
  */
 $( document ).ready(function(){
+    $("#mainContent").load('about.html');
     $("#contactCard").load('contactCard.html');
     $(".button-collapse").sideNav();
+    $('.tooltipped').tooltip({delay: 50});
 });
