60bfe29c95bd306a0b256d9555d6cf55b6a81f22
adunham95 <adunham95@gmail.com>
2017-01-08 19:31:55 -0500

    Working on adding reason json resume

--- js/main.js
+++ js/main.js
@@ -2,8 +2,21 @@
  * Created by apdun on 1/1/2017.
  */
 $( document ).ready(function(){
-    $("#mainContent").load('about.html');
+    $("#aboutMe").load('about.html');
+    $("#blog").load('blog.html');
+    $("#projects").load('projects.html');
+    $("#resume").load('resume.html');
     $("#contactCard").load('contactCard.html');
     $(".button-collapse").sideNav();
     $('.tooltipped').tooltip({delay: 50});
+    $('.collapsible').collapsible();
+
+    var myItems;
+    $.getJSON('resume.json', function (data) {
+        myItems = data.items;
+        console.log(myItems);
+    });
 });
+
+
+
