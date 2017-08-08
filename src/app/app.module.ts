ebf41c57260fd515cbf49d215e55ab9af5aa4cd4
adunham95 <Apd@21295>
2017-08-08 16:16:24 -0400

    Added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 import 'hammerjs';
 
 import {MdToolbarModule, MdSidenavModule, MdButtonModule, MdIconModule} from "@angular/material"
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { NoPageComponent } from './no-page/no-page.component';
+
+const appRoutes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: '**', component: NoPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    NoPageComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     MdToolbarModule,
     MdSidenavModule,
