0309a1b1251b700b9914469db7a2411a5fcec2e4
adunham95 <Apd@21295>
2017-08-08 23:14:40 -0400

    Added dummy blog post and loop

--- src/app/blog-page/blog-page.component.ts
+++ src/app/blog-page/blog-page.component.ts
@@ -7,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogPageComponent implements OnInit {
 
+  posts = [
+    {
+      title: 'Post One',
+      author: 'Adrian Dunham',
+      date: 'TBD 2017',
+      description: 'Bacon ipsum dolor amet reprehenderit eu elit nulla bacon do. Ullamco duis ea mollit. Meatloaf nisi tempor ipsum. Labore chicken prosciutto laborum excepteur mollit velit capicola shoulder. Reprehenderit kevin short ribs filet mignon doner esse beef ribs veniam spare ribs. Picanha shank cillum minim porchetta duis biltong aliqua cupidatat lorem occaecat eu. Minim kevin beef ribs cow aliqua.'
+    }
+
+    ];
   constructor() { }
 
   ngOnInit() {
