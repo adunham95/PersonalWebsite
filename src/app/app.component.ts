702656a899f9f2f3de46eca07e8aa67e2733d383
adunham95 <Apd@21295>
2017-08-16 22:14:59 -0400

    Working navigation Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Adrian Dunham';
+
+  /* Open */
+  openNav() {
+    document.getElementById("navigation").style.height = "100%";
+  }
+
+  /* Close */
+  closeNav() {
+    document.getElementById("navigation").style.height = "0%";
+  }
+
+  expand() {
+    console.log("Expanded");
+
+    document.getElementById('navigation').setAttribute("style", "height: 100%, visibility: visible");
+  }
+
 }
