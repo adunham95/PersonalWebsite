ce2dd22e3e1c5c9655746ab2cc648c4aa29452ea
adunham95 <Apd@21295>
2017-08-30 23:01:30 -0400

    Added database connection

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database');
 
--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-  database: 'mongodb://adunham:Apd@21295@ds163053.mlab.com:63053/personalsitedunham',
+  database: 'mongodb://adunham:apd21295@ds163053.mlab.com:63053/personalsitedunham',
   secret: 'yourseceret'
 }
