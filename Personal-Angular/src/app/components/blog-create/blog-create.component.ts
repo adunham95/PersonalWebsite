00ea98dcfaa7795fd7d6a211d623eab88af6e798
adunham95 <Apd@21295>
2017-09-04 23:04:35 -0400

    Set create blog element to create blog post

--- Personal-Angular/src/app/components/blog-create/blog-create.component.ts
+++ Personal-Angular/src/app/components/blog-create/blog-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {BlogService} from "../../services/blog.service";
+import {FlashMessagesService} from "angular2-flash-messages";
 
 @Component({
   selector: 'blog-create',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogCreateComponent implements OnInit {
 
-  constructor() { }
+  posts: any;
+  author: string;
+  content: string;
+  title: string;
+
+  user: any;
+
+  constructor(private blogService: BlogService, private flashMessage: FlashMessagesService) { }
 
   ngOnInit() {
+    const user = this.getUser();
+   this.author = user.name
   }
+
+  newPostSubmit(){
+    const post = {
+      title: this.title,
+      author: this.author,
+      body: this.content
+    };
+
+    this.blogService.savePost(post).subscribe(data =>{
+      if(data.success){
+        this.flashMessage.show(data.msg, {timeout: 5000});
+        this.title = "";
+        this.author = "";
+        this.content = "";
+      }
+      else{
+        this.flashMessage.show(data.msg, {timeout: 5000})
+      }
+    });
+  }
+
+  getUser(){
+    let user = JSON.parse(localStorage.getItem('user'));
+    return user
+  }
+
 
 }
