42427227f74bd9073e703444bc6fc2fbdcb3cb53
adunham95 <Apd@21295>
2017-09-11 22:52:38 -0400

    Added requests

--- Personal-Angular/src/app/services/blog.service.ts
+++ Personal-Angular/src/app/services/blog.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Http,  Headers} from "@angular/http";
-import 'rxjs/add/operator/map'
-import {tokenReference} from "@angular/compiler";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class BlogService {
--- Personal-Angular/src/app/services/resume.service.ts
+++ Personal-Angular/src/app/services/resume.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ResumeService {
 
   resume;
   isProd: boolean;
-  constructor() {
+
+  constructor(private http: Http) {
     this.isProd = false
   }
 
@@ -206,6 +209,13 @@ export class ResumeService {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
     let endPoint = this.getEndpoint('projects/saveproject');
+    return this.http.post(endPoint, project, {headers: headers}).map(res => res.json())
   }
 
+  getAllProjects(){
+    let headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    let endPoint = this.getEndpoint('projects/getprojects');
+    return this.http.get(endPoint, {headers: headers}).map(res => res.json())
+  }
 }
